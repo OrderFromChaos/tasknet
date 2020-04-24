6394ee6279d95f15cc40e50be97ce46fe9a43762
Syris Norelli <snore001@ucr.edu>
2020-04-24 12:01:38 -0700

    Add small note for Xeffect graphics

--- pages/staticgraphics/staticgraphics.py
+++ pages/staticgraphics/staticgraphics.py
@@ -2,4 +2,10 @@
 
 # TODO:
 # 1. Border around a region (x0, y0, xF, yF)
+    # Works with window size + .border(0) already, no need to implement
 # 2. Calendar starting on a day of the week and running for X days
+
+class displayMonth:
+    def __init__(self, window):
+        self.window = window
+    # TODO
