367b5110e4d9e8043a14c6132f29d0f527e82b9e
Syris Norelli <snore001@ucr.edu>
2020-04-24 09:37:17 -0700

    Write database reader

--- core/dbinterface.py
+++ core/dbinterface.py
@@ -1,13 +1,25 @@
 import json
-import core.dataclasses as dataclasses
+from core.dataclasses import *
 
 # Needs to know the "context" it's writing in.
 # This lets it select a folder in the style of data/<context>/todo.json
 # or the like.
 
-def writeTasks(L: List[dataclasses.Task], context: str, filename: str) -> None:
+def writeTasks(L: List[Task], context: str, filename: str) -> None:
     L = [x.serialize() for x in L]
     prettyjson = json.dumps(L, indent=4)
     
     with open('data/' + context + '/' + filename, 'w') as f:
         f.write(prettyjson)
+
+def readTasks(context, filename):
+    with open('data/' + context + '/' + filename, 'r') as f:
+        dataset = json.load(f)
+    
+    output = []
+    for entry in dataset:
+        t = Task()
+        t.deserialize(entry)
+        output.append(t)
+
+    return output
