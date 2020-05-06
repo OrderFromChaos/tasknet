a782e863ad3559eeab4bb19f81078235646c442b
Syris Norelli <snore001@ucr.edu>
2020-05-05 18:48:11 -0700

    Add natural language date presentation

--- pages/taskbrowser.py
+++ pages/taskbrowser.py
@@ -8,6 +8,7 @@ from collections import deque
 from ordered_set import OrderedSet
 
 import curses
+from datetime import timedelta
 
 class taskbrowser:
     def __init__(self, context):
@@ -76,20 +77,54 @@ class taskbrowser:
             rightwindow.border()
 
             selected = self.tasks[list(self.tasks.keys())[self.selected_option]]
-            selkeys = {
-                'name',
-                'expected_length',
-                'do by',
-                'due date',
-                'date added'
-            }
+
+            def naturaldate(truedate):
+                # Possible outputs:
+                # Jan 21, 2052 (if 11:59pm and date is not current year)
+                # Jan 21 (if 11:59pm and date is this year)
+                # Thursday (if the date is this week and 11:59pm)
+                # next Thursday (if date is next week and 11:59pm)
+                # X 10:00am (if the due date is not 11:59 on the previous options)
+                # TODO: Allow additional fuzziness (like "a month from now" or something)
+                #       in config
+                output = []
+                now = datetime.now()
+                # Assuming day of the week starts on Sunday
+                floorweek = truedate - timedelta(days=truedate.weekday()+1)
+                ceilweek = floorweek + timedelta(days=6, hours=23, minutes=59)
+                lastweek = floorweek - timedelta(days=7)
+                nextweek = ceilweek + timedelta(days=7)
+
+                if lastweek <= truedate <= floorweek:
+                    output.append('last')
+                    output.append(truedate.strftime(r'%A'))
+                elif floorweek <= truedate <= ceilweek:
+                    output.append(truedate.strftime(r'%A'))
+                elif ceilweek <= truedate <= nextweek:
+                    output.append('next')
+                    output.append(truedate.strftime(r'%A'))
+                else:
+                    output.append(truedate.strftime(r'%b %d'))
+                    if now.year != truedate.year:
+                        output.append(truedate.strftime(r'%Y'))
+
+                if truedate.strftime(r'%I:%M%p') != '11:59PM':
+                    output.append(truedate.strftime(r'%I:%M%p'))
+
+                return ' '.join(output)
 
             rightwindow.addstr(2, 2, f"Expected length: {selected.expectedlength}")
             if selected.duedate == None:
-                rightwindow.addstr(3, 2, f"Do by: {selected.doby}")
+                out = selected.doby
+                if out:
+                    out = naturaldate(out)
+                rightwindow.addstr(3, 2, f"Do by: {out}")
             else:
-                rightwindow.addstr(3, 2, f"Hard due date: {selected.duedate}", curses.COLOR_RED)
-            rightwindow.addstr(4, 2, f"Date added: {selected.dateadded}")
+                out = selected.duedate
+                if out:
+                    out = naturaldate(out)
+                rightwindow.addstr(3, 2, f"Hard due date: {out}", curses.COLOR_RED)
+            rightwindow.addstr(4, 2, f"Date added: {naturaldate(selected.dateadded)}")
 
 
             
