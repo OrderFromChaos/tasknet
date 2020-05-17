ce586ca41f4e3a2af0fd635746d790893c68a3ba
Syris Norelli <snore001@ucr.edu>
2020-05-17 12:15:54 -0700

    More notes on contextswitch

--- pages/contextswitch.py
+++ pages/contextswitch.py
@@ -3,6 +3,9 @@ class contextswitch:
     # "a" for adding context
     # "d" for context deletion UI (are you sure? currently has X tasks; show a few of them)
     #     no by default (of course)
+    # "e" to rename (input box needs option to prefill)
     # "s" -> alpha input with bisect_left on each step
-    # scrolling ui
+    #     shift-enter to immediately switch to said context
+    # scrolling ui, a la old school rotary phones
+    # TODO: add context indicator on other pages
     pass
