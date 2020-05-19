ecdeaa828e2ef98142de17f257133064f58c8071
Syris Norelli <snore001@ucr.edu>
2020-05-18 17:14:32 -0700

    Write custom textbox class
    The default textpad in curses is really bad, so I spun up my own class.
        1. It allows for non ^H backspacing
            (this is apparently an case of competing standards, so this may require later patches)
            https://stackoverflow.com/questions/47481955/python-curses-detecting-the-backspace-key
        2. It allows for char filtering (eg alphanumeric, no spaces)
            (this may be used for context adding)
        3. It allows for initialization with text already
            (this can be used to edit entries)
        4. It allows for proper border drawing
            (default textpad overwrites the borders)
        5. It doesn't leave a space on the end of the contents return
            (seriously, why does textbox do this??)

---
+++ core/customtextbox.py
@@ -0,0 +1,73 @@
+# Normal textpad doesn't allow for normal backspace
+# Normal textpad doesn't allow char filtering
+# Normal textpad doesn't allow for repositioning (afaik),
+    # meaning window borders get overwritten
+# Normal textpad doesn't let you do ctrl+q to exit 
+# Normal textpad doesn't let you initialize with contents
+# Normal textpad doesn't remove the tailing space by default
+# (The custom version does let you do these things!)
+    # (TODO: Except for ctrl+q to exit)
+
+# Old textpad interface:
+    # textwindow = curses.newwin(nrows, ncols, startx, starty)
+    # box = textpad.Textbox(textwindow, insert_mode=True)
+    # contents = box.edit()
+    # del textwindow
+    # contents = contents.strip()
+    # self.tasks[self.addtask.uid].name = contents
+
+import curses
+from string import ascii_lowercase, ascii_uppercase
+
+class CustomTextbox:
+    def __init__(self,
+                 window, # Window to draw textbox in
+                 contents=[], # Pre-initializing textbox
+                 charfilter=set(), # If non-empty, only these chars are accepted
+                 border=False # If True, textbox is verti-centered and shifted right 1
+                 ):
+        self.window = window
+        self.contents = list(contents)
+        if charfilter:
+            if isinstance(charfilter, list):
+                charfilter = set(charfilter)
+            self.filter = charfilter
+        else:
+            self.filter = (ascii_lowercase 
+                           + r"""`~1!2@3#4$5%6^7&8*9(0)-_=+[{]}\|;:'",<.>/? """
+                           + ascii_uppercase)
+            self.filter = set(self.filter)
+        self.border = border
+    def edit(self):
+        last = None # TODO: Use this for catching control sequences (eg CTRL)
+
+        while True:
+            # 1. Draw window
+            self.window.clear()
+            self.window.refresh()
+
+            if not self.border:
+                self.window.addstr(0, 0, ''.join(self.contents))
+            else:
+                self.window.border()
+                max_y, max_x = self.window.getmaxyx()
+                self.window.addstr(max_y//2, 1, ''.join(self.contents))
+
+            # 2. Key processing
+            c = self.window.getch()
+            if c == ord('\n'):
+                break
+            elif c == 127:
+                if self.contents:
+                    self.contents.pop()
+            # Todo: catch ctrl/alt patterns
+            
+            if chr(c) not in self.filter:
+                continue
+            
+            self.contents.append(chr(c))
+        
+        return self.contents
+
+
+        
