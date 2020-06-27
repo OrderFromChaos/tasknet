badceb1054e8d9ea54cb10bd8239e63f0d1a92c4
Syris Norelli <snore001@ucr.edu>
2020-06-27 01:14:13 -0700

    Add a few tasks to _GLOBALTODO

--- _GLOBALTODO.py
+++ _GLOBALTODO.py
@@ -1,3 +1,5 @@
 # Support vim bindings globally
 # Switch to using OrderedDict anywhere where order matters
 # Can curses handle modifier keys? e.g. ctrl+h
+# when autoscheduling: prioritize by position swapping (similar to task list)
+# change task color when they're already autoscheduled
