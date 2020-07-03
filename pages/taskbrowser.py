3dbb4fdcbfea5eede80a71d31ae5662b0e4ca930
Syris Norelli <snore001@ucr.edu>
2020-07-03 11:22:09 -0700

    Move naturaldate to core/utility

--- pages/taskbrowser.py
+++ pages/taskbrowser.py
@@ -1,17 +1,18 @@
-# needs to accept recursive todos
-# display todo information on the side
 # needs to be scrollable (todo list might be long)
 # needs to cut todo names down if they're too long
 
-from core.dbinterface import *
-from collections import deque
-from ordered_set import OrderedSet
+# Repo-internal libraries
+from core.dbinterface import *                  # Reading and writing tasks
+from core.utility import naturaldate            # Making datetimes nice and readable 
+from core.customtextbox import CustomTextbox    # Backspace-supported editing
 
-import curses
-from core.customtextbox import CustomTextbox
-from datetime import timedelta
-from copy import deepcopy
-import time
+# Standard libraries
+import curses                       # Primary renderer
+from collections import deque       # BFS
+from copy import deepcopy           # List deepcopy for DFS iteration
+
+# External dependencies
+from ordered_set import OrderedSet  # Surprisingly not a part of python stdlib
 
 DEBUG = True # TODO: Make this a main passed SETTINGS dict later on
 
@@ -112,42 +113,6 @@ class taskbrowser:
             rightwindow.border()
 
             selected = self.tasks[list(self.tasks.keys())[self.selected_option]]
-
-            def naturaldate(truedate):
-                # Possible outputs:
-                # Jan 21, 2052 (if 11:59pm and date is not current year)
-                # Jan 21 (if 11:59pm and date is this year)
-                # Thursday (if the date is this week and 11:59pm)
-                # next Thursday (if date is next week and 11:59pm)
-                # X 10:00am (if the due date is not 11:59 on the previous options)
-                # TODO: Allow additional fuzziness (like "a month from now" or something)
-                #       in config
-                output = []
-                now = datetime.now()
-                # Assuming day of the week starts on Sunday
-                nowmorn = datetime(now.year, now.month, now.day)
-                floorweek = nowmorn - timedelta(days=(nowmorn.weekday()+1) % 7)
-                ceilweek = floorweek + timedelta(days=6, hours=23, minutes=59)
-                lastweek = floorweek - timedelta(days=7)
-                nextweek = ceilweek + timedelta(days=7)
-
-                if lastweek <= truedate < floorweek:
-                    output.append('last')
-                    output.append(truedate.strftime(r'%A'))
-                elif floorweek <= truedate <= ceilweek:
-                    output.append(truedate.strftime(r'%A'))
-                elif ceilweek < truedate <= nextweek:
-                    output.append('next')
-                    output.append(truedate.strftime(r'%A'))
-                else:
-                    output.append(truedate.strftime(r'%b %d'))
-                    if now.year != truedate.year:
-                        output.append(truedate.strftime(r'%Y'))
-
-                if truedate.strftime(r'%I:%M%p') != '11:59PM':
-                    output.append(truedate.strftime(r'%I:%M%p'))
-
-                return ' '.join(output)
 
             rightwindow.addstr(2, 2, f"Expected length: {selected.expectedlength}")
             if selected.duedate == None:
